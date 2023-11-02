4b840f9d6184c6c2613246e4af0f80a60ec1eb18
Greg Plumbly <greg.plumbly@gmail.com>
2023-11-02 16:55:29 +0000

    customise.

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "IND chat",
+  title: "Creative care chat",
   description: "Chatbot with custom knowledge base",
 };
 
